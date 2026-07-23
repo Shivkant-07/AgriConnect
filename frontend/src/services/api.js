ae4a048aa76eef178cbdc6675032f168c5e6b799
shivkant kurmi <pshivkantpatel@gmail.com>
2026-07-23 09:50:17 +0530

    Updated backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:  "https://agriconnect-7z6l.onrender.com/api",
 });
 
 export default api;
